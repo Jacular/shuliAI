152616764f4c5c64e9ea58808aab7e6edf4498c9
hy_wanglin <wanglin@hua-cloud.com.cn>
2025-03-09 00:12:17 +0800

    [feat]完整项目结构

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.sqldelightPlugin)
-    alias(libs.plugins.ktorPlugin)
 }
 
 kotlin {
